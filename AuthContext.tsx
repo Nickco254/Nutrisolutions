9d7bf57f89bba8f8f4c29923d7dd2655c42d349d
Nickco254 <115428676+Nickco254@users.noreply.github.com>
2025-07-24 02:45:04 +0300

    Hooks
    Hooks

---
+++ AuthContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext<{ user: User | null }>({ user: null });
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => setUser(u));
+    return () => unsubscribe();
+  }, []);
+  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
